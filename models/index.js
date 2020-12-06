297094cec1ae30406538f3ab16ba71fedee208d9
HeatherNev88 <heather.alv88@yahoo.com>
2020-12-05 20:21:09 -0800

    dependencies added, database created and api routes

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = (mongoose) => {
+    return {
+        Book: require("./book")(mongoose),
+    }
+  };
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
-const IOPORT = process.env.IOPORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
-
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', (client) => {
 
-});
-server.listen(IOPORT);
 
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -20,6 +14,8 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+} else {
+  app.use(express.static('client/public'));
 }
 
 mongoose.connect(
@@ -33,15 +29,15 @@ mongoose.connect(
 ).then(() => {
   // Define API routes here
   app.use('/api', require('./utils/api')(mongoose));
-  console.log('Database is connected');
+  
 
   // Send every other request to the React app
   // Define any API routes before this runs
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/src/public/index.html'));
   });
 
   app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
+    console.log(`App running on port: " ${PORT}!`);
   });
 });
